7aba46c812e0b59c38bc0bc2362c6c7b20c6afca
José Ramos <joseramoslewis@gmail.com>
2023-07-05 11:04:06 -0400

    update modelo

--- README.md
+++
@@ -1,5 +0,0 @@
-# proyecto-1-modulo-7
-
-superuser: jose pass: asd123
-
-Debería existir un usuario "pedro" con la pass "martini123"
--- actividad1/principal/models.py
+++ actividad1/principal/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    nombre = models.CharField(max_length=50)
 
+    def str(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    STATUS_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En progreso'),
+        ('completada', 'Completada'),
+    )
+
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_limite = models.DateField()
+    estado = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendiente')
+    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.titulo
